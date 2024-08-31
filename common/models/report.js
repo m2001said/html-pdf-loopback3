2a87682bb14dc2f760a4c4422f112282c22ad07d
m2001said <ms277712@gmail.com>
2024-08-31 09:03:13 +0300

    adding locales and dynamic pages 1 ,2 ,3

--- common/models/report.js
+++ common/models/report.js
@@ -2,6 +2,8 @@
 const ejs = require("ejs");
 const utils = require("../../utils");
 const moment = require("moment");
+const ar = require("../../locales/ar.json");
+const en = require("../../locales/en.json");
 
 module.exports = function (report) {
   report.remoteMethod("generate", {
@@ -12,9 +14,18 @@ module.exports = function (report) {
 
   report.generate = async (res) => {
     try {
+      const locale = ar; // You can switch to `en` for English content
+      let program;
+      let programId = 3;
+
+      if (programId === 2) {
+        program = locale.REPORT.SELF_CONFIDENCE;
+      } else if (programId === 3) {
+        program = locale.REPORT.LEADERSHIP;
+      }
+
       const data = {
-        date: moment().format("DD/MM/YYYY"),
-        username: "محمد خالد",
+        // -----all images---------
         heroLogo: utils.IMGToURI("hero-logo.png"),
         rightCheck: utils.IMGToURI("right-check.png"),
         backgroundSmallTitle: utils.IMGToURI("background-small-title.jpg"),
@@ -30,6 +41,23 @@ module.exports = function (report) {
         note3: utils.IMGToURI("note3.png"),
         note4: utils.IMGToURI("note4.png"),
         treePapers: utils.IMGToURI("tree-papers.png"),
+
+        // ---data---
+        date: moment().format("DD/MM/YYYY"),
+        username: "محمد خالد",
+
+        title: program.TITLE,
+        subtitle: program.SUBTITLE,
+        aboutTitle: locale.REPORT.TITLE_ABOUT,
+        resultDescriptionTitle: locale.REPORT.TITLE_RESULT_DESCRIPTION,
+        pointsTitle: locale.REPORT.TITLE_POINTS,
+        pointsRestTitle: locale.REPORT.TITLE_POINTS_REST,
+        comparisonTitle: locale.REPORT.TITLE_COMPARISON,
+        developmentRecommendationsTitle:
+          locale.REPORT.TITLE_DEVELOPMENT_RECOMMENDATIONS,
+        notesTitle: locale.REPORT.TITLE_NOTES,
+
+        cards: program.CARDS,
       };
 
       const html = await ejs.renderFile(
